64f6047bb55d81467a47084e59a8684c02ed789a
Anton_Dyrdin <a.dyrdin@oblakogroup.ru>
2024-08-23 03:44:12 +0300

    wip

--- fill_templete.py
+++ fill_templete.py
@@ -1,8 +1,8 @@
 import win32com.client
 import pandas as pd
 import os
-import re
-import qrcode
+import pyperclip
+from utils import generate_qr_code, wrap_text
 
 # подправить слово "Диаозон"  в русских шильдах. Разумеется на Диапозон.
 
@@ -15,13 +15,15 @@ import qrcode
 
 # распихать по папкам в новом порядке
 
-lang = 'eng'
-lang_rus = 'инг'
-shield_type = 2
-shield_type_text = "small" if shield_type == 2 else "big"
-MAX_TEXT_WIDTH = 4.330688976377953 if shield_type == 1 else 4.724389763779527
+LANG = 'eng'
+LANG_RUS = 'инг' if LANG == 'eng' else 'рус'
+SHIELD_WITHOUT_QR = 1
+SHIELD_WITH_QR = 2
+shield_type = SHIELD_WITH_QR
+shield_type_text = "small" if shield_type == SHIELD_WITH_QR else "big"
+MAX_TEXT_WIDTH = 4.330688976377953 if shield_type == SHIELD_WITHOUT_QR else 4.724389763779527
 MAX_LINE_LENGTH = 50
-SHOW_UI = True
+SHOW_UI = shield_type == SHIELD_WITH_QR
 ONLY_WITH_SERIAL_NUMBERS = True
 
 # секундомер
@@ -34,42 +36,13 @@ def getTime():
     return offset
 #####################################
 
-def wrap_text(text, max_width):
-    lines = []
-    words = re.split(r'( +|-)', text)
-
-    current_line = ""
-    for i in range(0, len(words)):
-        if len(current_line) + len(words[i]) > max_width:
-            # следующее слово выйдет за границу
-            lines.append(current_line.strip().replace('\n', '\r'))
-            current_line = words[i]
-        elif len(words) > i + 2 and words[i + 1] == '-' and len(current_line) + len(words[i]) + 1 + len(words[i + 1]) > max_width:
-            if len(current_line) + len(words[i]) + 1 > max_width:
-                lines.append(current_line.strip().replace('\n', '\r'))
-                current_line = words[i]
-            else:
-                current_line += words[i] + '-'
-                lines.append(current_line.strip().replace('\n', '\r'))
-                current_line = ''
-        elif current_line != '' or (words[i] != '-' and words[i] != ' '):
-            current_line += words[i]
-
-    if current_line:
-        lines.append(current_line.strip().replace('\n', '\r'))
-        
-    for line in lines:
-        print('"' +line+'"')
-    print('')
-    return "\r".join(lines)
-
 def get_text(row, max_width):
     text = ''
-    number_key = 'Код модели' if lang == 'rus' else 'Model number'
+    number_key = 'Код модели' if LANG == 'rus' else 'Model number'
     model_number = row[number_key]
 
-    if shield_type == 1:
-        if lang == 'eng':
+    if shield_type == SHIELD_WITHOUT_QR:
+        if LANG == 'eng':
             # Type 1 ENG
             text = f"Instrument tag no: {row['Instrument tag no']}\r" if row['Instrument tag no'] == row['Instrument tag no'] else f"Instrument tag no:"
             text += wrap_text(f"{number_key}: {model_number}", max_width) + "\r"
@@ -89,8 +62,8 @@ def get_text(row, max_width):
             text += f"Номер заказа: {row['Номер заказа']}\r" if row['Номер заказа'] == row['Номер заказа'] else f"Номер заказа"
             text += f"Серийный номер: {row['Серийный номер']}" if row['Серийный номер'] == row['Серийный номер'] else f"Серийный номер:"
     
-    if shield_type == 2:
-        if lang == 'eng':
+    if shield_type == SHIELD_WITH_QR:
+        if LANG == 'eng':
             # Type 2 ENG
             text = f"Instrument tag no: {row['Instrument tag no']}\r" if row['Instrument tag no'] == row['Instrument tag no'] else f"Instrument tag no:"
             text += wrap_text(f"Instrument service: {row['Instrument service']}", max_width) + '\r' if row['Instrument service'] == row['Instrument service'] else f"Instrument service:"
@@ -114,22 +87,24 @@ def process_template(cdr_file, xlsx_file, output_folder):
     corel.Visible = SHOW_UI
     
     getTime()
+    # Главный цикл
+    ##############################################################################
     for index, row in data.iterrows():
         tag_no = row['Instrument tag no']
 
         if row['Serial number'] == row['Serial number'] or not ONLY_WITH_SERIAL_NUMBERS:
-            try:
-                src = open(cdr_file, 'rb')
-                dst = open(os.path.join(output_folder, f"{tag_no}_{shield_type_text}_{lang}.cdr"), 'wb')
-                try:
-                    dst.write(src.read())
-                finally:
-                    dst.close()
-            finally:
-                src.close()
+            # try:
+            #     src = open(cdr_file, 'rb')
+            #     dst = open(os.path.join(output_folder, f"{tag_no}_{shield_type_text}_{LANG}.cdr"), 'wb')
+            #     try:
+            #         dst.write(src.read())
+            #     finally:
+            #         dst.close()
+            # finally:
+            #     src.close()
 
-            doc = corel.OpenDocument(os.path.join(output_folder, f"{tag_no}_{shield_type_text}_{lang}.cdr"))
-            
+            # doc = corel.OpenDocument(os.path.join(output_folder, f"{tag_no}_{shield_type_text}_{LANG}.cdr"))
+            doc = corel.OpenDocument(cdr_file)
             
             for page in doc.Pages:
                 for shape in page.Shapes:
@@ -144,20 +119,38 @@ def process_template(cdr_file, xlsx_file, output_folder):
                             shape.Text.Story.Text = get_text(row, max_line_length)
 
                         if shape.SizeWidth > MAX_TEXT_WIDTH:
-                            raise 'shape.SizeWidth > MAX_TEXT_WIDTH !'
+                            raise Exception('shape.SizeWidth > MAX_TEXT_WIDTH !')
                         
                         shape.Fill.ApplyNoFill()
                         shape.Outline.Width = 0.003
 
             doc.ActivePage.Shapes.All().ConvertToCurves()
+            
+            if shield_type == SHIELD_WITH_QR:
 
-            doc.Save()
+                generate_qr_code(tag_no.replace('-',''))
+                
+                pyperclip.copy(os.path.join("D:\dev\shields\qr_codes", f"{tag_no.replace('-','')}.svg"))
+                
+                input("Ожидание импорта QR кода...")
+            
+                for page in doc.Pages:
+                    page.Shapes.First.SetPosition(1.8300354330708661, 0.5905511811023622)
+                    page.Shapes.First.SetSize(1.1811023622047243, 1.1811023622047243)
+                    page.Shapes.First.Fill.ApplyNoFill()
+                    page.Shapes.First.Outline.Width = 0.003
+
+                pyperclip.copy(os.path.join(output_folder, f"{tag_no}_{shield_type_text}_{LANG}.ai"))
+                input("Ожидание сохранения файла...")
+            # Закрытие файла
+            ##############################################################################
+            # doc.Save()
             doc.Close()
-            os.remove(os.path.join(output_folder, f"Резервная_копия_{tag_no}_{shield_type_text}_{lang}.cdr"))
-            print(f"{str(index)}/{str(data.shape[0])}. Осталось {str((getTime() * (data.shape[0] - (index + 1)))/60)[0:5]} мин. Итерация: {str(getTime())[0:5]} сек..")
+            # os.remove(os.path.join(output_folder, f"Резервная_копия_{tag_no}_{shield_type_text}_{LANG}.cdr"))
+            print(f"{str(index+1)}/{str(data.shape[0])}. Осталось {str((getTime() * (data.shape[0] - (index + 1)))/60)[0:5]} мин. Итерация: {str(getTime())[0:5]} сек..")
 
-cdr_file =  f"D:\dev\shields\шильд тип {shield_type} {lang_rus}\шильд тип {shield_type} {lang_rus} ( текст еще не в кривых).cdr"
+cdr_file =  f"D:\dev\shields\шильд тип {shield_type} {LANG_RUS}\шильд тип {shield_type} {LANG_RUS} ( текст еще не в кривых).cdr"
 xlsx_file = "dataset.xlsx"
-output_folder = f"D:\dev\shields\Type {shield_type} {lang.upper()}"
+output_folder = f"D:\dev\shields\Type {shield_type}\{LANG.upper()}"
 
 process_template(cdr_file, xlsx_file, output_folder)
---
+++ utils.py
@@ -0,0 +1,51 @@
+import os
+import qrcode
+import qrcode.image.svg
+import re
+
+def generate_qr_code(data):
+    factory = qrcode.image.svg.SvgPathImage
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=0,
+    )
+
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    img = qr.make_image(image_factory=factory)
+
+    img.save(f"qr_codes/{data}.svg")
+
+    return os.path.join("D:\dev\shields\qr_codes", f"{data}.svg")
+
+def wrap_text(text, max_width):
+    lines = []
+    words = re.split(r'( +|-)', text)
+
+    current_line = ""
+    for i in range(0, len(words)):
+        if len(current_line) + len(words[i]) > max_width:
+            # следующее слово выйдет за границу
+            lines.append(current_line.strip().replace('\n', '\r'))
+            current_line = words[i]
+        elif len(words) > i + 2 and words[i + 1] == '-' and len(current_line) + len(words[i]) + 1 + len(words[i + 1]) > max_width:
+            if len(current_line) + len(words[i]) + 1 > max_width:
+                lines.append(current_line.strip().replace('\n', '\r'))
+                current_line = words[i]
+            else:
+                current_line += words[i] + '-'
+                lines.append(current_line.strip().replace('\n', '\r'))
+                current_line = ''
+        elif current_line != '' or (words[i] != '-' and words[i] != ' '):
+            current_line += words[i]
+
+    if current_line:
+        lines.append(current_line.strip().replace('\n', '\r'))
+        
+    for line in lines:
+        print('"' +line+'"')
+    print('')
+    return "\r".join(lines)
